66ef8a5eeee22200d3b515ae868c59eb5763b9d1
Simon San <14062932+simonsan@users.noreply.github.com>
2021-10-23 15:17:59 +0200

    adding types and toml

---
+++ data/type.rs
@@ -0,0 +1,55 @@
+enum EloRange {
+    2kPlus,
+    1k4Plus,
+    800Plus,
+}
+
+enum Category {
+    CastingRanked(EloRange)
+    CastingTournaments,
+    OrganizingTournaments,
+    CommunityGames,
+    PovContent(EloRange),
+    LearningResources,
+}
+
+enum LanguageShortCode {
+    En,
+    De,
+    Fr,
+    Esp,
+    It,
+    Por,
+    Rus,
+    Other(String),
+}
+
+enum GameShortCode {
+    Aoe1,
+    Aoe2,
+    Aoe3,
+    Aoe4,
+    Aom,
+    AoeO,
+    Other(String),
+}
+
+type StreamerUrl = String;
+
+enum Platform {
+    Twitch(StreamerUrl),
+    Youtube(StreamerUrl),
+    Facebook(StreamerUrl),
+    Douyu(StreamerUrl),
+    Discord(StreamerUrl),
+}
+
+struct Elo(u64);
+
+struct ContentCreator {
+    id: usize,
+    name: String,
+    platforms: Vec<(Platform, Vec<Category>, Vec<LanguageShortCode>, Vec<GameShortCode>)>,
+    elo: Option<Elo>,
+    aoc_ref_name: Option<String>,
+}
